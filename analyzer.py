0e0dc054aeb42460bc080917fe126f7a0195179f
Ivo Cavelti <ivo.cavelti@bluewin.ch>
2022-01-19 08:16:02 +0100

    Refactor code and add other charts

--- analyzer.py
+++ analyzer.py
@@ -3,34 +3,56 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-message_regex = re.compile(r"^\[(\d{1,2}\.\d{1,2}\.\d{4}\,\s\d{2}\:\d{2})\]\s([\w\s]+)\:\s(.+)$")
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+class MessageFormatter:
 
-message_data_frames = []
+    message_regex = re.compile(r"^\[(\d{1,2}\.\d{1,2}\.\d{4}\,\s\d{2}\:\d{2})\]\s([\w\s]+)\:\s(.+)$")
+
+    @staticmethod
+    def chat_to_data_frame(chat):
+        data = []
+        for message in chat:
+            result = re.search(MessageFormatter.message_regex, message)
+            if result:
+                datetime = pd.to_datetime(result.group(1), format='%d.%m.%Y, %H:%M')
+                sender = result.group(2)
+                message = result.group(3)
+                data.append([datetime, sender, message])
+
+        return pd.DataFrame(data, columns=['datetime', 'sender', 'message'])
+
+class ChartPlotter:
+
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    @staticmethod
+    def plot_weekly_distribution_bar_chart(dataset):
+        data_set = chat_data_frame['weekday'].value_counts(sort=False).reindex(range(7), fill_value=0).sort_index()
+        data_set.plot(kind="bar")
+        plt.xticks(range(7), ChartPlotter.weekdays)
+        plt.show()
+
+    @staticmethod
+    def plot_message_share_pie_chart(dataset):
+        data_set = chat_data_frame['sender'].value_counts(sort=False)
+        data_set.plot(kind="pie")
+        plt.show()
+
+    @staticmethod
+    def plot_hourly_distribution_bar_chart(dataset):
+        data_set = chat_data_frame['hour_of_the_day'].value_counts(sort=False).reindex(range(24), fill_value=0).sort_index()
+        data_set.plot(kind="bar")
+        plt.show()
+
+
+
+def generate_derived_columns(data_frame):
+    data_frame["weekday"] = chat_data_frame["datetime"].apply(lambda date: date.weekday())
+    data_frame["hour_of_the_day"] = chat_data_frame["datetime"].apply(lambda date: date.hour)
+
 
 with open("./messages.txt") as chat_log:
-    print(type(chat_log))
-    for message in chat_log:
-        result = re.search(message_regex, message)
-        if result:
-            datetime = pd.to_datetime(result.group(1), format='%d.%m.%Y, %H:%M')
-            message_data_frames.append(
-                pd.DataFrame(
-                    {
-                        "datetime": [datetime],
-                        "sender": [result.group(2)],
-                        "message": [result.group(3)]
-                    }
-                )
-            )
-
-
-
-chat_data_frame = pd.concat(message_data_frames)
-chat_data_frame["weekday"] = chat_data_frame["datetime"].apply(lambda date: date.weekday())
-chat_data_frame["hour_of_the_day"] = chat_data_frame["datetime"].apply(lambda date: date.hour)
-
-data_set = chat_data_frame['weekday'].value_counts(sort=False).reindex(range(7), fill_value=0).sort_index()
-data_set.plot(kind="bar")
-plt.xticks(range(7), weekdays)
-plt.show()
+    chat_data_frame = MessageFormatter.chat_to_data_frame(chat_log)
+    generate_derived_columns(chat_data_frame)
+    ChartPlotter.plot_weekly_distribution_bar_chart(chat_data_frame)
+    ChartPlotter.plot_message_share_pie_chart(chat_data_frame)
+    ChartPlotter.plot_hourly_distribution_bar_chart(chat_data_frame)
